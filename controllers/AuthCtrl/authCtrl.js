ceeb41e668b210547a91f549b9b164b153c41827
marcosgallardi <gallardimarcos98@gmail.com>
2023-10-27 12:19:22 -0300

    login del usuario

--- controllers/AuthCtrl/authCtrl.js
+++ controllers/AuthCtrl/authCtrl.js
@@ -1,10 +1,16 @@
 const sequelize = require("../../DB_connection");
+const bcrypt = require("bcryptjs");
 
 const authCtrl = async (name, password) => {
   try {
     const result = await sequelize.query("SELECT * FROM sys.user$");
 
     const user = result[0].find((user) => user.NAME === name);
+    const encryptedPassword = user.PASSWORD;
+    console.log(encryptedPassword);
+
+    const isPasswordValid = await bcrypt.compare(password, encryptedPassword);
+    console.log(isPasswordValid);
     console.log(user);
     return user;
   } catch (error) {
--- handlers/AuthHandler/authHandler.js
+++ handlers/AuthHandler/authHandler.js
@@ -3,7 +3,7 @@ const authCtrl = require("../../controllers/AuthCtrl/authCtrl");
 const authHandler = async (req, res) => {
   try {
     const { name, password } = req.body;
-    const  result  = await authCtrl("MFARZANI", password);
+    const  result  = await authCtrl(name, password);
     console.log(result,"acaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     res.status(200).json(result);
   } catch (error) {
