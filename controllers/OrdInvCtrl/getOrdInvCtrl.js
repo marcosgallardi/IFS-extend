411ecb595794aa99b749faf236f5c8bf3e18e192
marcosgallardi <gallardimarcos98@gmail.com>
2023-11-10 13:03:24 -0300

    finalizando las vistas de orden y factura

--- controllers/OrdInvCtrl/getOrdInvCtrl.js
+++ controllers/OrdInvCtrl/getOrdInvCtrl.js
@@ -1,10 +1,11 @@
-
+// vista de la orden SNM_ORDEN_VENTAS_VIEW
+//vista de la factura SNM_FACTURAS_CLIENTES_VIEW
 const getOrdInvCtrl = async () => {
     let sequelize = global.sequelize;
     
     console.log(sequelize,"exist sequelize")
   try {
-    const data = await sequelize.query("SELECT * FROM INVOICE_TAB");
+    const data = await sequelize.query("SELECT * from SNM_FACTURAS_CLIENTES_VIEW");
     console.log(data, "data");
     return data;
   } catch (error) {
