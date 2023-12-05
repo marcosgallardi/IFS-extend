59a211a93c676324eed20534f809cebee82549b4
marcosgallardi <gallardimarcos98@gmail.com>
2023-12-05 14:17:09 -0300

    ruta de la orden casi lista

--- controllers/OrdInvCtrl/getOrderCtrl.js
+++ controllers/OrdInvCtrl/getOrderCtrl.js
@@ -2,7 +2,7 @@ const getOrderCtrl = async (cliente) => {
   let sequelize = global.sequelize;
   try {
     const data = await sequelize.query(
-      `SELECT * FROM SNM_ORDEN_VENTA_VIEW  WHERE CUSTOMER_NO = '${cliente}'`
+      `SELECT * FROM SNM_ORDEN_VENTA_VIEW  WHERE CUSTOMER_NO = '${cliente}' AND S.DATES > TO_DATE('01/10/2023', 'DD/MM/YYYY') AND S.STATE != 'cancelado'`
     );
     return data[0];
   } catch (error) {
