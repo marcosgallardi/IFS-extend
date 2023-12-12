f06aae5863f50aaf54604aeca8fde671a80c985b
marcosgallardi <gallardimarcos98@gmail.com>
2023-12-12 13:19:34 -0300

    rutas de las imagenes

--- controllers/RRHHCtrl/rrhhNovCtrlPut.js
+++ controllers/RRHHCtrl/rrhhNovCtrlPut.js
@@ -1,9 +1,14 @@
-
-
-const rrhhNovCtrlPut = (id, path) => {
+const rrhhNovCtrlPut = async (id, url) => {
+  try {
     let sequelize = global.sequelize;
-    
-
+    let data = await sequelize.query(
+      `UPDATE SNM_RRHH_NOV SET url = '${url}' WHERE id = ${id}`,
+      { type: sequelize.QueryTypes.UPDATE }
+    );
+    return data;
+  } catch (error) {
+    throw error.message;
+  }
 };
 
 module.exports = rrhhNovCtrlPut;
--- handlers/RRHH/rrhhNovHandlerPut.js
+++ handlers/RRHH/rrhhNovHandlerPut.js
@@ -1,10 +1,14 @@
 const rrhhNovCtrlPut = require("../../controllers/RRHHCtrl/rrhhNovCtrlPut");
 
-const rrhhNovHandlerPut = (res, req) => {
+const rrhhNovHandlerPut = async (res, req) => {
   try {
-    const { id, path } = req.body;
-    const data = rrhhNovCtrlPut(id, path);
-    res.status(200).json(data);
+    const { id, url } = req.body;
+    const data = await rrhhNovCtrlPut(id, url);
+    if (data) {
+      res.status(200).json({ message: "Modificado con exito" });
+    } else {
+      res.status(400).json({ message: "No se pudo modificar" });
+    }
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
